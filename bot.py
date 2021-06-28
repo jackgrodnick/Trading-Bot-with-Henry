764b7448fb6681345e0d9d3bbfb4a078c984c963
Jack Grodnick <jackgrodnick@jacks-mbp.lan>
2021-06-27 19:50:27 -0500

    The program will never terminate now once it is run and will continually run the bot every minute.

--- bot.py
+++ bot.py
@@ -1,99 +1,108 @@
 from config import *
 import json
-#import time
+import time
 import alpaca_trade_api as tradeapi
 from tradingview_ta import TA_Handler, Interval, Exchange
 
-alpaca_endpoint = 'https://paper-api.alpaca.markets'
+while 1 == 1:
+
+    alpaca_endpoint = 'https://paper-api.alpaca.markets'
 
 
-api = tradeapi.REST(API_KEY, SECRET_KEY, 'https://paper-api.alpaca.markets', api_version='v2')
+    api = tradeapi.REST(API_KEY, SECRET_KEY, alpaca_endpoint, api_version='v2')
 
 
 
-#print(account.status)
+    #print(account.status)
 
-#print(account)
+    #print(account)
 
-lines = open("data/qqqm.csv").readlines()
+    lines = open("data/qqqm.csv").readlines()
 
-#for line in lines:
-    #print(line)
+    #for line in lines:
+        #print(line)
 
-symbolsinqqqm = [line.split(",")[2] for line in lines][1:-90]
+    symbolsinqqqm = [line.split(",")[2] for line in lines][1:-90]
 
-#print(symbolsinqqqm)
+    #print(symbolsinqqqm)
 
-clock = api.get_clock()
+    clock = api.get_clock()
 
 
-def buyorders(ticker, quantity):
-    api.submit_order(
-    symbol=ticker,
-    qty=quantity,
-    side='buy',
-    type='market',
-    time_in_force='gtc'
-)
+    def buyorders(ticker, quantity):
+        api.submit_order(
+        symbol=ticker,
+        qty=quantity,
+        side='buy',
+        type='market',
+        time_in_force='gtc'
+    )
 
-def sellorders(ticker, quantity):
-    api.submit_order(
-    symbol=ticker,
-    qty=quantity,
-    side='sell',
-    type='market',
-    time_in_force='gtc'
-)
-#clock.is_open
+    def sellorders(ticker, quantity):
+        api.submit_order(
+        symbol=ticker,
+        qty=quantity,
+        side='sell',
+        type='market',
+        time_in_force='gtc'
+    )
+    #clock.is_open
 
-#position = tradeapi.REST(API_KEY, SECRET_KEY, 'https://paper-api.alpaca.markets/v2/positions/AAPL')
+    #position = tradeapi.REST(API_KEY, SECRET_KEY, 'https://paper-api.alpaca.markets/v2/positions/AAPL')
 
-#position = api.get_position('AAPL')
+    #position = api.get_position('AAPL')
 
-#print(position)
+    #print(position)
 
-#asset = api.get_asset('AAPL')
-#print(asset.exchange)
+    #asset = api.get_asset('AAPL')
+    #print(asset.exchange)
 
-while clock.is_open:
-    for symbol in symbolsinqqqm:
+    printcount = 0
 
-        account = api.get_account()
-        #asset = tradeapi.REST(API_KEY, SECRET_KEY, 'https://paper-api.alpaca.markets/v2/assets/{}'.format(symbol.strip()))
-        asset = api.get_asset('{}'.format(symbol.strip()))
-        #print(asset)
+    while clock.is_open:
+        for symbol in symbolsinqqqm:
+            printcount += 1
+            account = api.get_account()
+            #asset = tradeapi.REST(API_KEY, SECRET_KEY, 'https://paper-api.alpaca.markets/v2/assets/{}'.format(symbol.strip()))
+            asset = api.get_asset('{}'.format(symbol.strip()))
+            #print(asset)
 
-        stock = TA_Handler(
-            symbol='{}'.format(symbol.strip()),
-            screener="america",
-            exchange=asset.exchange,
-            interval=Interval.INTERVAL_1_DAY
-        )
+            stock = TA_Handler(
+                symbol='{}'.format(symbol.strip()),
+                screener="america",
+                exchange=asset.exchange,
+                interval=Interval.INTERVAL_1_DAY
+            )
 
-        if stock.get_analysis().summary['RECOMMENDATION'] == "BUY" and float(account.cash) > 0:
-            buyorders('{}'.format(symbol.strip()), 1)
+            if stock.get_analysis().summary['RECOMMENDATION'] == "BUY" and float(account.cash) > 0:
+                buyorders('{}'.format(symbol.strip()), 1)
 
-        try:
-            position = api.get_position('{}'.format(symbol.strip()))
-            if position.symbol == symbol.strip() and stock.get_analysis().summary['RECOMMENDATION'] == "SELL":
-                sellorders('{}'.format(symbol.strip()), int(position.qty))
-        except:
-            continue
-    #add this back later maybe.
-    #if float(account.cash) <= 0:
-        #break
+            try:
+                position = api.get_position('{}'.format(symbol.strip()))
+                if position.symbol == symbol.strip() and stock.get_analysis().summary['RECOMMENDATION'] == "SELL":
+                    sellorders('{}'.format(symbol.strip()), int(position.qty))
+            except:
+                continue
+        #add this back later maybe.
+        #if float(account.cash) <= 0:
+            #break
         
 
 
-#tick = str(input("enter the ticker you want to buy ")).strip()
+    #tick = str(input("enter the ticker you want to buy ")).strip()
 
-#quantity = str(input("enter the quantity of stock you want to buy ")).strip()
-account = api.get_account()
-#position = tradeapi.REST(API_KEY, SECRET_KEY, 'https://paper-api.alpaca.markets/v2/positions')
-#buyorders(tick, int(quantity))
-#print(account.cash)
-#print(json.dumps(tradeapi.REST(API_KEY, SECRET_KEY, 'https://paper-api.alpaca.markets/v2/positions/aapl}', api_version='v2')))
-
-print('Percent Change of Account Today: ' + str((float(account.last_equity) - float(account.equity))/float(account.equity)) + '%')
+    #quantity = str(input("enter the quantity of stock you want to buy ")).strip()
+    account = api.get_account()
+    #position = tradeapi.REST(API_KEY, SECRET_KEY, 'https://paper-api.alpaca.markets/v2/positions')
+    #buyorders(tick, int(quantity))
+    #print(account.cash)
+    #print(json.dumps(tradeapi.REST(API_KEY, SECRET_KEY, 'https://paper-api.alpaca.markets/v2/positions/aapl}', api_version='v2')))
 
 
+
+
+    if printcount > 0:
+        print('Percent Change of Account Today: ' + str((float(account.last_equity) - float(account.equity))/float(account.equity)) + '%')
+
+
+    time.sleep(60)
